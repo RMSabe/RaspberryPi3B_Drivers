597b366f8a5daabafd1a2b49b8be85974e727f17
RMSabe <69878824+RMSabe@users.noreply.github.com>
2022-11-13 16:04:49 -0300

    Update MMU32_usr.h

--- MMU32_usr.h
+++ MMU32_usr.h
@@ -6,8 +6,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//Returns true if "mmu_init()" has already been called.
 bool mmu_is_active(void);
+//Initializes MMU procedure.
+//This function must be called before calling any other functions in this header.
+//Returns true if initialization is successful.
 bool mmu_init(void);
+
 uint32_t mmu_get_phys_from_virt(void *virtaddr);
 void *mmu_get_virt_from_phys(uint32_t physaddr);
 
