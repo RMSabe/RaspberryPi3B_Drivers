2884c4bb3e935e2a2954c421673c3cb80272cdaa
RMSabe <69878824+RMSabe@users.noreply.github.com>
2022-11-13 16:07:57 -0300

    Update SYSTIMER_Ctrl.h

--- SYSTIMER_Ctrl.h
+++ SYSTIMER_Ctrl.h
@@ -4,8 +4,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//Returns true if "systimer_init()" has already been called.
 bool systimer_is_active(void);
+//Initializes SYSTIMER procedure.
+//This function must be called before calling any other function in this header.
+//Returns true if initialization is successful.
 bool systimer_init(void);
+
 bool systimer_timer_match_occurred(uint8_t timer_num);
 uint32_t systimer_get_counter_value_l32(void);
 uint32_t systimer_get_counter_value_h32(void);
